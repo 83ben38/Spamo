92482d96336a0a326cf44c17cc817b5b6c205ec1
Ben <bb14738@ms.wasatch.edu>
2024-11-09 11:29:07 -0700

    Initial Commit

---
+++ Spamo/SpamoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpamoApp.swift
+//  Spamo
+//
+//  Created by Ben Blodgett on 11/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SpamoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
